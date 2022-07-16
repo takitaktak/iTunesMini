eae082bf81effd89bd27d26cc7d9a3a06d276cb3
Katreena C. Bacalso <kat.bacalso@gmail.com>
2022-07-16 22:52:41 +0800

    Refactored Code

--- iTunesMini/Views/SearchTracksListView.swift
+++ iTunesMini/Views/SearchTracksListView.swift
@@ -17,59 +17,9 @@ struct SearchTracksListView: View {
             ScrollView {
                 LazyVGrid(columns: columns, spacing: 15) {
                     ForEach(viewModel.filteredTracks, id: \.id) { trackVM in
-                        VStack(alignment: .leading) {
-                            // Artwork
-                            AsyncImage(url: URL(string: trackVM.artwork)) { phase in
-                                switch phase {
-                                case .empty:
-                                    ProgressView()
-                                    
-                                case .success(let image):
-                                    image
-                                        .resizable()
-                                        .scaledToFill()
-                                    
-                                default:
-                                    Image(systemName: "music.note")
-                                }
-                            }
-                            .frame(height: 100, alignment: .center)
-                            .clipped()
-                            
-                            // Favorite Button
-                            .overlay(alignment: .topTrailing) {
-                                Button {
-                                    print("Favorite tapped!")
-                                    viewModel.toggleFavorite(for: trackVM)
-                                } label: {
-                                    Image(systemName: trackVM.isFavorite ? "heart.fill" : "heart")
-                                        .resizable()
-                                        .scaledToFit()
-                                        .foregroundColor(trackVM.isFavorite ? Color.red : Color.gray)
-                                }
-                                .frame(width: 20, height: 20)
-                                .padding(5)
-                            }
-                            
-                            // Track Name
-                            Text(trackVM.name)
-                                .font(.system(size: 14, weight: .semibold))
-                                .lineLimit(1)
-                            
-                            HStack {
-                                Text(trackVM.genre)
-                                    .font(.system(size: 12, weight: .light))
-                                    .foregroundColor(Color.gray)
-                                    .lineLimit(1)
-                                
-                                Spacer()
-                                
-                                Text(trackVM.price)
-                                    .font(.system(size: 14, weight: .bold))
-                                    .foregroundColor(Color.gray)
-                                    .lineLimit(1)
-                            }
-                        }.padding(5)
+                        TrackCell(track: trackVM) { trackVM in
+                            viewModel.toggleFavorite(for: trackVM)
+                        }
                     }
                 }.padding(10)
             }
--- iTunesMini/Views/TrackCell.swift
+++ iTunesMini/Views/TrackCell.swift
@@ -8,12 +8,64 @@
 import SwiftUI
 
 struct TrackCell: View {
-    @EnvironmentObject var track: TrackViewModel
+    let track: TrackViewModel
+    let toggleFavoriteAction: (TrackViewModel) -> Void
     
     var body: some View {
-        VStack {
-//            TrackThumbnailView()
-            TrackKeyDetailView(track: track)
-        }
+        VStack(alignment: .leading) {
+            // Artwork
+            AsyncImage(url: URL(string: track.artwork)) { phase in
+                switch phase {
+                case .empty:
+                    ProgressView()
+                    
+                case .success(let image):
+                    image
+                        .resizable()
+                        .scaledToFill()
+                    
+                default:
+                    Image(systemName: "music.note")
+                }
+            }
+            .frame(height: 100, alignment: .center)
+            .clipped()
+            
+            // Favorite Button
+            .overlay(alignment: .bottomTrailing) {
+                Text(track.price)
+                    .padding(2)
+                    .font(.system(size: 13, weight: .bold))
+                    .foregroundColor(Color.white)
+                    .lineLimit(1)
+                    .background(Color.black.opacity(0.5))
+                    .cornerRadius(7)
+                    .padding(5)
+            }
+            
+            // Track Name
+            Text(track.name)
+                .font(.system(size: 14, weight: .semibold))
+                .lineLimit(1)
+            
+            HStack {
+                Text(track.genre)
+                    .font(.system(size: 12, weight: .light))
+                    .foregroundColor(Color.gray)
+                    .lineLimit(1)
+                
+                Spacer()
+                
+                Button {
+                    print("Favorite tapped!")
+                    toggleFavoriteAction(track)
+                } label: {
+                    Image(systemName: track.isFavorite ? "heart.fill" : "heart")
+                        .font(.system(size: 17))
+                        .foregroundColor(track.isFavorite ? Color.red : Color.gray)
+                }
+                
+            }
+        }.padding(5)
     }
 }
